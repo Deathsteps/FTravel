08eed6d159e2734337dc97901ab0390b04fb3d6e
Deathsteps <lovely_dreamer@126.com>
2015-12-01 14:59:36 +0800

    PureRenderMixin

--- public/js/components/NotFoundPage.jsx
+++ public/js/components/NotFoundPage.jsx
@@ -1,18 +1,18 @@
 var React = require('react');
-var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
+var PureRenderMixin = require('react-addons-pure-render-mixin');
 
 var NotFoundPage = React.createClass({
 	mixins: [PureRenderMixin],
 
-    render: function () {
-    	var data = this.props.data;
-        return (
+  render: function () {
+  	var data = this.props.data;
+    return (
 			<div>
-	       		<h1 style="color:red;">Page Not Found</h1>
-	       		<p>"Not Found the target page, please check the address or return home page."</p>
-	      	</div>
-        );
-    }
+				<h1 style="color:red;">Page Not Found</h1>
+				<p>"Not Found the target page, please check the address or return home page."</p>
+			</div>
+		);
+  }
 });
 
 module.exports = NotFoundPage;
