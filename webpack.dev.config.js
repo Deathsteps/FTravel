04d1112ceb2a61a24a91b7f851ba1a9ae2680eb2
Deathsteps <lovely_dreamer@126.com>
2015-12-02 01:40:08 +0800

    HMR with webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,19 +1,23 @@
+var webpack = require("webpack");
 var path = require("path");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context: path.join(__dirname, "public", "js"),
   entry: [
+    "webpack-dev-server/client?http://localhost:9000", 
+    "webpack/hot/dev-server",
     "app"
   ],
   output: {
+    publicPath: "http://localhost:9000/",
     path: path.join(__dirname, "public", "assets"),
     filename: "bundle.js"
   },
   module: {
     loaders: [{
       test: /\.jsx$/,
-      loader: "jsx-loader?harmony"
+      loader: "react-hot!jsx-loader?harmony"
     },{
       test: /\.css$/,
       loader: "css-loader"
@@ -28,6 +32,7 @@ module.exports = {
     modulesDirectories: ["node_modules"]
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin("[name].css")
   ]
 };
